5ea6d34c3428dd931bfb165ae5d7e874a1899d4f
T@RuN UpAdHyAy <116960857+metarun97@users.noreply.github.com>
2023-01-07 22:34:26 +0530

    Add files via upload

---
+++ My JS Practice/Variables.js
@@ -0,0 +1,47 @@
+// Variables
+
+// let username = 'John'
+// let age = 20
+// let message = 'Hello'
+
+// let username = 'John',age = 20,message = 'Hello'
+
+// Underline comments are egnored bye the output portion
+
+// (a-) /* */  This is a multiline comment
+// (b-) // This is the single line comment
+
+let name = 'John'
+name = 'Peter'
+console.log(name)
+
+let name1 = 'John'
+let name2 = 'abcd'
+console.log(name1)
+
+// (Variables naming)
+/*The name must contain only letters,digits and symbols like $ and _ */
+
+// NOTE=> First character cannot be a digit.
+// let 123    (Error found)
+// let $ = 2  NO error found)
+// let_ = 20  (No error found)
+
+let $ = 'Tarun'
+console.log($)
+// let 2a = 'Hello'  (Invalid error)
+// let myname =      (Invalid error)
+
+// let myfirstName   (Way to write M is small=> Camel case)
+// let MyfirstName   (Way to write M is capital=> Pascal case)
+
+let myname = 'Messy'
+let MYNAME = 'M Bappe'
+let MyName = 'Pele'
+let myName = 'Ronaldo'
+// It is case sensitive language.
+
+// I you want to declare constant unchanging variables.
+const myBirthday = ('08/10/2000')
+myBirthday // It gives the date as shown in the input. 
+
---
+++ My JS Practice/datatypes.js
@@ -0,0 +1,60 @@
+                                                       /* DATA TYPES*/
+
+
+
+// There are total 8 data types in javascript.
+let message = 'Hello'
+message= 20
+// It can be as name and number it can't be change.
+
+// Number
+let num = 10
+console.log(num)
+
+let num2 = 20.2345
+console.log(num2)
+
+// let num = 10
+console.log(num*2)
+
+// Numbers data types also includes special numeric values.
+// In Infinity,-Infinity,NaN
+
+// let num = 10
+console.log(1/0)
+
+// let num = 10
+console.log('Hello'/0)
+
+console.log(2*2)  // (4) Multipy 
+console.log(2**3) // (8) Squae to square the number
+console.log(NaN**0) // (1) is output
+
+                                        /*(BigInt) in Java Script => means (Big Integer)*/
+
+/*If the range of an integer is +-(2 power 53-1) */
+
+const bigInt = 6345867
+console.log(bigInt)  // (6345867) is output
+
+                                                     /*Strings in Java Script */
+
+let string = 'Hello'
+console.log(string) // (Hello)
+
+let str = 'Hello'
+let str1 = 'Hi'
+let str2 = " & now"
+let str3 = 'bye'
+console.log(str,str1,str2,str3) // Hello Hi & now bye
+
+// Double and single quotes those are same.
+// Back ticks are special quotes for entendef functionality.
+
+let name = 'John'
+console.log("hello " + name)  // hello Johns
+console.log(`hello  ${name}`) // hello John
+
+
+
+
