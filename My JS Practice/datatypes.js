4206c018973d322bda2f3278b9a22e9a97a9fe53
T@RuN UpAdHyAy <116960857+metarun97@users.noreply.github.com>
2023-01-08 13:16:40 +0530

    Update datatypes.js

--- My JS Practice/datatypes.js
+++ My JS Practice/datatypes.js
@@ -64,4 +64,65 @@ console.log(2<3) // true
 // It is a boolean
 
 
+                                                     /*Null data types */
+
+let age = null
+console.log(age) // null
+// It represents nothing an empty value or unknown values.
+
+age = 12
+console.log(12)  // (12) output
+
+                                                   /* Undefined */
+
+// Note=> The meaning of undefined is not assigned.
+
+// let age ;
+Console.log(age)  // Undefined
+
+age = 10
+Console.log(age)  // Undefined
+
+                                                   /* Objects and symbols */
+
+/* Premetive and unpremetive */                                                   
+/* All the other types are called as premetive because they can hold only one value  */
+/* Object can contain any type of data base */ 
+
+                                                   /* Objects and symbols */
+ 
+// let name = 'John'
+console.log(typeof name) // (String)
+                                                                      
+console.log(typeof 2n)   // (bigInt)                             
+
+console.log(typeof 2<5) // (False)
+
+console.log(typeof false)  
+console.log(typeof true)
+// Both are boolean values.
+
+                                                // String Conversion
+
+/* Type Conversion*/
+  
+let value = true
+console.log(typeof value)  // boolean
+
+value = String(value)
+console.log(typeof value)  // string
+
+                                                /* Number conversion */
+
+// let str = "1234"
+console.log(typeof str)   // string
+
+// let num = Number(str)
+console.log(typeof num)    // number
+
+
+// let age = Number('This is a string')
+console.log(age)            // NaN
+console.log(typeof age)    // number
+
 
